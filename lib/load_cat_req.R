e30052e1751a37b015e19e0d2431f2d0a9190c1e
hectormarioromer <hectormarioromer@gmail.com>
2022-03-03 21:31:29 -0400

    Fix col_types definition.

--- lib/load_cat_req.R
+++ lib/load_cat_req.R
@@ -10,7 +10,7 @@ suppressPackageStartupMessages(library(readr))
 load_cat_req <- function(filename = cat_req_file) {
         # Source: snipe-it custom assets report
         d <- read_csv(filename,
-                col_types = "ffnn")
+                col_types = "fffnn")
         
         return(d)
 }
